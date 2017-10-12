232f160e6d1aea6082e3fbfd9570791b17368335
Katelyn Waddell <waddellk0423@gmail.com>
2017-10-11 19:08:20 -0500

    adding db connection

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from flask import Flask, request, render_template
-import cgi
-import os
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['DEBUG']=True
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://build-a-blog:tellmeeverything@localhost:8889/build-a-blog'
+app.config['SQLAlchemy_ECHO']=True
+
+db=SQLAlchemy(app)
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    post_body=db.Column(db.String(1000))
+
+    def __init__(self, title, post_body):
+        self.name=name
+        self.title=title
+        self.post_body=post_body
 
 form = """ 
 
@@ -15,10 +28,14 @@ def index():
 
 @app.route("/add-entry", methods=['GET','POST'])
 def add_entry():
-    return render_template('addentry.html')
+    if request.method=='GET':
+        return render_template('addentry.html')
+    else:
+        return render_template('entry.html')
 
 @app.route("/entry", methods=['GET'])
 def entry():
     return render_template('entry.html')
 
-app.run()
+if __name__=='__main__':
+    app.run()
